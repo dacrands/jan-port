239ea01eaa84999f8bc498f0e686e40b7c51ab81
da crands <davecrands@gmail.com>
2019-08-12 22:10:47 -0400

    Add index header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,32 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+import "../styles/main.scss"
+
+export default () => (
+  <main className="container">
+  <header class="header">
+    <div className="header__title">
+        <h1 className="text--title">David Crandall
+        </h1>
+    </div>
+
+    <div className="header__content">
+      <p class="text--red">
+        REST. MVC. CRUD. Web APIs. HTTP. Authentication. Relational Databases. Containerization (Docker).
+      </p>
+      <p>
+        Strong JavaScript Kowledge. Responsive Web Design. Experienced with ReactJS. Semantic HTML. Modular CSS (BEM). Accessibility Conscious.
+      </p>
+      <p>
+        <a href="asdfasf">Github</a>
+        <a href="asdfasdf">Email</a>
+        <a href="asdfasdf">Resume</a>
+      </p>
+    </div>
+
+  </header>
+  <section class="section">    
+    <h2 class="text--title text--red">Projects</h2>   
+  </section>
+</main>
+)
