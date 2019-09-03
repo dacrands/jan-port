9ab7d13c5ac48c899d3cc83c1ce2539d88f671e4
da crands <davecrands@gmail.com>
2019-09-02 20:22:33 -0400

    Add success page

---
+++ src/pages/success.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import "../styles/main.scss"
+
+export default () => <main className="center container">
+  <div className="success">
+    <h1 className="text__title text--red">Thanks for the message!</h1>
+
+    <p>I'll read it and get back to you</p>
+    <Link to="/">Go back home</Link>
+  </div>
+</main>
