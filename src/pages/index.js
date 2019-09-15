d6345d90162f78bd3618bfe99741d74fa2f7a116
da crands <davecrands@gmail.com>
2019-09-15 15:02:52 -0400

    Update pages to use Layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,15 @@ import React from "react"
 
 import "../styles/main.scss"
 
+import Layout from "../components/Layout"
 import Header from "../components/Header"
 import Portfolio from "../components/Portfolio"
 import Contact from "../components/Contact"
 
 export default ({ data }) => (
-  <main className="container">
+  <Layout>
     <Header />
     <Portfolio />
     <Contact />
-  </main>
+  </Layout>
 )
--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,17 +1,20 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import Layout from  "../components/Layout"
+
 import "../styles/main.scss"
 
 const Thanks = () => (
-  <div className="center container">
-    <div className="success">
-      <h1 className="text__title text--red">Thanks for the message!</h1>
-
-      <p>I'll read it and get back to you</p>
-      <Link to="/">Go back home</Link>
+  <Layout>
+    <div className="center">
+      <div className="success">
+        <h1 className="text__title text--red">Thanks for the message!</h1>
+        <p>I'll read it and get back to you</p>
+        <Link to="/">Go back home</Link>
+      </div>
     </div>
-  </div>
+  </Layout>
 )
 
 export default Thanks
